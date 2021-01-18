f21ecc99886b6827facf116bed3627dc794d31a3
rong <1141591465@qq.com>
2021-01-18 12:46:38 +0800

    add

---
+++ cube.js
@@ -0,0 +1,26 @@
+// Cube.js configuration options: https://cube.dev/docs/config
+const PostgresDriver = require("@cubejs-backend/postgres-driver");
+const CubeStoreDriver = require("@cubejs-backend/cubestore-driver")
+module.exports = {
+    devServer: true,
+    dbType: ({ dataSource } = {}) => {
+        return 'postgres';
+    },
+    telemetry: false,
+    apiSecret: "b2db7688e328d316d85e924d8b9a0737d87162a9f2cf36325f1ca0ae08dbdaa990520750847226cf8dcbb1fb4c07afe1087c7cb03b8f9f05b9abad3eb4058f3f",
+    driverFactory: ({ dataSource } = {}) => {
+        return new PostgresDriver({
+            user: "postgres",
+            database: "postgres",
+            password: "dalong",
+            port: 5432,
+            host: "127.0.0.1",
+            readOnly: true
+        });
+    },
+    externalDbType: 'cubestore',
+    externalDriverFactory: () => new CubeStoreDriver({
+        host: '127.0.0.1',
+        port: 3306
+    })
+};
